08a36241c010d5fb85002b616c7b2dfb8afbde36
Snehansh09-03 <shivanshdubeyraj@gmail.com>
2025-10-15 20:03:11 +0530

    Create main.tf

---
+++ terraform-files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0bc691261a82b32bc"
+  instance_type = "t2.micro"
+  key_name = "ubuntu"
+  vpc_security_group_ids = ["sg-0c7b73297c52807d3"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("Staragile.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/Banking-proj/terraform-files/ansibleplaybook.yml"
+     }
+  }
